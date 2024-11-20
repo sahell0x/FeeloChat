eaf5cdd7be4abc96400d35e7dd7284d20423dbdf
sahell0x <sahil.sk.khan6872@gmail.com>
2024-11-20 19:38:26 +0530

    sign up page styling

--- frontend/src/components/ui/SignUp.jsx
+++ frontend/src/components/ui/SignUp.jsx
@@ -1,7 +1,8 @@
 import Modal from "@/util/Modal";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Input } from "@/components/ui/input";
-import feeloChatLogo from "../../assets/IMG-20240813-WA0001-removebg.png";
+import feeloChatLogo from "../../assets/FeeloChatLogo.png";
+
 import { Button } from "./button";
 
 function SignUp() {
@@ -10,21 +11,21 @@ function SignUp() {
       <div className="flex flex-col gap-10 items-center justify-center">
         <div className="flex justify-center items-center flex-col">
           <div className="flex flex-col justify-center items-center mb-5">
-            {/* <h1 className="text-5xl font-bold md:text-6xl">FeeloChat</h1> */}
 
-            <img className="w-[35%] h-[35%] pl-2" src={feeloChatLogo} alt="" />
-            <p className="font-semibold">Let your expressions speak for you !!</p>
+            <img className="w-[60%] h-[60%] sm:w-[35%] h-[35%]" src={feeloChatLogo} alt="feeloChat Logo" />
           </div>
-          <Tabs defaultValue="account" className="w-[50vw] flex justify-center flex-col items-center">
+          <Tabs defaultValue="signup" className="w-[50vw] flex justify-center flex-col items-center">
             <TabsList className="w-[300px] flex justify-around">
-              <TabsTrigger value="signup" className="w-[50%]">signup</TabsTrigger>
-              <TabsTrigger value="signin" className="w-[50%]">signin</TabsTrigger>
+              <TabsTrigger value="signup" className="w-[50%]">sign up</TabsTrigger>
+              <TabsTrigger value="signin" className="w-[50%]">sign in</TabsTrigger>
             </TabsList>
             <TabsContent value="signup" className="w-[300px]">
                 <Input  placeholder="Username" className="mb-2"></Input>
                 <Input  placeholder="Password" className="mb-2"></Input>
                 <Input  placeholder="Confirm Password" className="mb-2"></Input>
-                <Button className="w-[100%]">signup</Button>
+                <Button style={{backgroundColor:"#1e2864"}} className="w-[100%] mb-2">sign up</Button>
+                <Button style={{backgroundColor:"#e5b238"}} className="w-[100%]">Try as Guest
+                </Button>
               
             </TabsContent >
             <TabsContent value="signin" className="w-[300px]">
