2981c01308500ae9f62a2fab3eb176e14e8be711
sahell0x <sahil.sk.khan6872@gmail.com>
2024-11-20 20:27:19 +0530

    state managment in sign up

--- frontend/src/components/ui/SignUpTabContent.jsx
+++ frontend/src/components/ui/SignUpTabContent.jsx
@@ -1,16 +1,44 @@
 import { Input } from "@/components/ui/input";
 import { Button } from "./button";
+import { useState } from "react";
 
 function SignUpTabContent() {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
   return (
     <div>
-      <Input placeholder="Username" className="mb-2"></Input>
-      <Input placeholder="Password" className="mb-2"></Input>
-      <Input placeholder="Confirm Password" className="mb-5"></Input>
-      <Button style={{ backgroundColor: "#1e2864" }} className="w-[100%] mb-2 rounded-full">
+      <Input
+        value={userName}
+        onChange={(e) => setUserName(e.target.value)}
+        placeholder="Username"
+        className="mb-2"
+      ></Input>
+
+      <Input
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Password"
+        className="mb-2"
+      ></Input>
+      <Input
+        value={confirmPassword}
+        onChange={(e) => setConfirmPassword(e.target.value)}
+        placeholder="Confirm Password"
+        className="mb-5"
+      ></Input>
+
+      <Button
+        style={{ backgroundColor: "#1e2864" }}
+        className="w-[100%] mb-2 rounded-full"
+      >
         Sign up
       </Button>
-      <Button style={{ backgroundColor: "#e5b238" }} className="w-[100%] rounded-full">
+      <Button
+        style={{ backgroundColor: "#e5b238" }}
+        className="w-[100%] rounded-full"
+      >
         Try as Guest
       </Button>
     </div>
