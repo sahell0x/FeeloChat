27a8ceae1fb2ad48929711891d236c120352d591
sahell0x <sahil.sk.khan6872@gmail.com>
2024-11-20 20:30:07 +0530

    state managment in sign in

--- frontend/src/components/ui/SignInTabContent.jsx
+++ frontend/src/components/ui/SignInTabContent.jsx
@@ -1,15 +1,32 @@
-import { Input } from './input';
-import { Button } from './button';
+import { Input } from "./input";
+import { Button } from "./button";
+import { useState } from "react";
+
 function SignInTabContent() {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <div>
-      <Input placeholder="Username" className="mb-2"></Input>
-      <Input placeholder="Password" className="mb-5"></Input>
-      <Button style={{ backgroundColor: "#1e2864" }} className="w-[100%] mb-2 rounded-full">
+      <Input
+        value={userName}
+        onChange={(e) => setUserName(e.target.value)}
+        placeholder="Username"
+        className="mb-2"
+      ></Input>
+      <Input
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Password"
+        className="mb-5"
+      ></Input>
+      <Button
+        style={{ backgroundColor: "#1e2864" }}
+        className="w-[100%] mb-2 rounded-full"
+      >
         Sign in
       </Button>
-      </div>
-  )
+    </div>
+  );
 }
 
-export default SignInTabContent
+export default SignInTabContent;
