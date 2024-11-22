96678cbd0209b2f53176ce848fdd135e3686d616
sahell0x <sahil.sk.khan6872@gmail.com>
2024-11-22 18:55:22 +0530

    updated types

--- backend/middlewares/signUpMiddleware.js
+++ backend/middlewares/signUpMiddleware.js
@@ -4,7 +4,9 @@ const signUptypes = z.object({
     email: z.string().email(),
     password:z.string().min(8),
     firstName: z.string.max(30).min(2),
-    lastName: z.string.max(30).min(2)
+    lastName: z.string.max(30).min(2),
+    img:z.string().optional(),
+
 });
 
 export default signUptypes;
