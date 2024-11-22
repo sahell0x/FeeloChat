6bcdf8e2b08256253c724ffe452d80fcd65fa9c5
sahell0x <sahil.sk.khan6872@gmail.com>
2024-11-22 17:43:27 +0530

    initialized server

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,30 @@
-console.log("hello world");
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+const dbUrl = process.env.DB_URL;
+
+app.use(cors({
+    origin:[process.env.ORIGIN],
+    methods:["GET","POST","PUT","PATCH","DELETE"],
+    credentials:true,
+}));
+
+app.use(cookieParser());
+app.use(express.json());
+
+app.listen(port,()=>{
+    console.log(`server is runnig at http://localhost:${port}`);
+});
+
+mongoose.connect(dbUrl).then(()=>{
+    console.log("connected successfully");
+}).catch((err)=>{
+    console.log("error while connecting");
+})
