b9f2ad072343c79e3d75947177679ac00b401ba2
sahell0x <sahil.sk.khan6872@gmail.com>
2024-11-24 15:45:07 +0530

    updated user schema

--- backend/controllers/signInController.js
+++ backend/controllers/signInController.js
@@ -40,7 +40,8 @@ const signInController = async (req,res)=>{
             id:user._id,
             firstName:user.firstName,
             lastName:user.lastName,
-            img:user.img
+            img:user.img,
+            profileSetup:user.profileSetup,
         });
 
     }catch(e){
