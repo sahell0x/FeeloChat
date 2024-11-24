782b762563805cb696ca290ce5456f09b3c2c6d4
sahell0x <sahil.sk.khan6872@gmail.com>
2024-11-24 15:39:09 +0530

    sign in handler

--- frontend/src/components/ui/SignInTabContent.jsx
+++ frontend/src/components/ui/SignInTabContent.jsx
@@ -2,9 +2,30 @@ import { Input } from "./input";
 import { Button } from "./button";
 import { useState } from "react";
 import { PasswordInput } from "./PasswordInput";
+import {emailValidator} from "@/util/validator.js";
+import { useToast } from "@/hooks/use-toast";
+import apiClient from "@/lib/api-client";
+import { SIGNIN_ROUTE } from "@/util/constants";
+
+
 function SignInTabContent() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const { toast } = useToast();
+
+  const handleSignIn = async ()=>{
+    if(!emailValidator(email)){
+      toast({variant: "destructive",
+        title: "Please enter valid Email address.",});
+    }else if(!password.length){
+      toast({variant: "destructive",
+        title: "password can not be empty.",});
+    }else{
+      const response = await apiClient.post(SIGNIN_ROUTE,{email,password},{withCredentials:true});
+
+      console.log(response);
+    }
+  }
   return (
     <div>
       <Input
@@ -21,6 +42,10 @@ function SignInTabContent() {
         className="mb-2"
       ></PasswordInput>
       <Button
+
+        onClick={()=>{
+          handleSignIn();
+        }}
         
         className="w-[100%] mb-2 rounded-full"
       >
