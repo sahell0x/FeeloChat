950b28fbdd7ab40adc11a9949789cbaccb015243
sahell0x <sahil.sk.khan6872@gmail.com>
2024-11-24 20:07:51 +0530

    default nevigation if to profile if user is not seted up their profile

--- frontend/src/components/ui/SignInTabContent.jsx
+++ frontend/src/components/ui/SignInTabContent.jsx
@@ -6,12 +6,14 @@ import {emailValidator} from "@/util/validator.js";
 import { useToast } from "@/hooks/use-toast";
 import apiClient from "@/lib/api-client";
 import { SIGNIN_ROUTE } from "@/util/constants";
+import { useNavigate } from "react-router-dom";
 
 
 function SignInTabContent() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const { toast } = useToast();
+  const navigate = useNavigate();
 
   const handleSignIn = async ()=>{
     if(!emailValidator(email)){
@@ -23,7 +25,14 @@ function SignInTabContent() {
     }else{
       const response = await apiClient.post(SIGNIN_ROUTE,{email,password},{withCredentials:true});
 
-      console.log(response);
+      if(response.status===200){
+        if(!response.data.profileSetup){
+          navigate("/profile");
+        }else{
+          navigate("/chat");
+        }
+      }
+      
     }
   }
   return (
