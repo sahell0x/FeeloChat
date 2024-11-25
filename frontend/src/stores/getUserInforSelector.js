c29212f4c879d5151dfd5bb0590497c38816ac19
sahell0x <sahil.sk.khan6872@gmail.com>
2024-11-25 22:27:28 +0530

    added get user info selector to fetch user info if it is not allready there

--- frontend/src/stores/getUserInforSelector.js
+++ frontend/src/stores/getUserInforSelector.js
@@ -1,9 +1,23 @@
-import { selector } from "recoil";
+import { selector, useRecoilValue } from "recoil";
 import userInfoAtom from "./userInfoAtom";
+import apiClient from "@/lib/api-client";
+import { USERINFO_ROUTE } from "@/util/constants";
 
 const getUserInfoSelector = selector({
     key:"getUserInfoSelector",
     get: async ({get})=>{
-        
+        const userInfo = useRecoilValue(userInfoAtom);
+        if(userInfo) return userInfo;
+
+        try{
+            
+            const response = await apiClient.get(USERINFO_ROUTE);
+            if(response.status===200) return response.data;
+            return null;
+
+
+        }catch{
+            return null;
+        }
     }
 })
