df3c43025a6feb8807bd20b6ff4a43bdd76d218e
sahell0x <sahil.sk.khan6872@gmail.com>
2024-11-25 21:05:10 +0530

    redirect to the profile page if user havent done profile initialization

--- frontend/src/pages/chat/Chat.jsx
+++ frontend/src/pages/chat/Chat.jsx
@@ -1,5 +1,23 @@
+import { useToast } from "@/hooks/use-toast";
+import userInfoAtom from "@/stores/userInfoAtom";
+import { useEffect } from "react"
+import { useNavigate } from "react-router-dom";
+import { useRecoilValue } from "recoil";
 
 function Chat() {
+  const userInfo = useRecoilValue(userInfoAtom);
+  const navigate = useNavigate();
+  const {toast} = useToast();
+
+  useEffect(()=>{
+
+    if(!userInfo.profileSetup){
+      toast({variant: "destructive",
+        title: "Please setup Profile first to sign Up",});
+        navigate("/profile");
+    }
+
+  },[userInfo,navigate]);
   return (
     <div>
       chat route
