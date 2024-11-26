5bc528bfa66ada4f7df2b87ad1b37169ad945793
sahell0x <sahil.sk.khan6872@gmail.com>
2024-11-26 21:22:47 +0530

    auth route

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -3,6 +3,7 @@ import signUpMiddleware from "../middlewares/signUpMiddleware.js";
 import signUpController from "../controllers/signUpController.js";
 import signInMiddleware from "../middlewares/signInMiddleware.js";
 import signInController from "../controllers/signInController.js";
+import authMiddleware from "../middlewares/authMiddleware.js";
 
 
 const authRoutes = Router();
@@ -10,6 +11,8 @@ const authRoutes = Router();
 authRoutes.post("/signup",signUpMiddleware,signUpController);
 
 authRoutes.post("/signin",signInMiddleware,signInController);
+
+authRoutes.get("/user",authMiddleware);
 
 
 export default authRoutes;
