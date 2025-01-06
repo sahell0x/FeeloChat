e4c2e49a7fb8fa0434c292b06f5d2a86249c3586
sahell0x <sahil.sk.khan6872@gmail.com>
2025-01-06 19:40:00 +0530

    updated user profile controller

--- backend/controllers/updateUserProfileController.js
+++ backend/controllers/updateUserProfileController.js
@@ -1,5 +1,28 @@
-const updateUserProfileController = async (req,res)=>{
+import {StatusCode} from "status-code-enum";
+import profileNameTypes from "../types/profileNameTypes.js";
+import User from "../models/userModel.js";
 
+const updateUserProfileController = async (req,res)=>{
+    try{
+        const userId = req.userId;
+    const userProfileData = req.body;
+    const {success} = profileNameTypes.safeParse(userProfileData);
+
+    if(!success){
+       return res.status(StatusCode.ClientErrorBadRequest).json({
+            message:"invalid inputs.",
+        });
+    }
+   
+    const response = await User.findByIdAndUpdate(userId,{...userProfileData,profileSetup:true});
+
+    return res.status(StatusCode.SuccessAccepted).json({
+        message:"user profile updated successfully",
+    });
+
+    }catch(e){
+     return res.status(StatusCode.ServerErrorInternal).json({message:"internal server error."});
+    }
 }
 
 export default updateUserProfileController;
---
+++ backend/types/profileNameTypes.js
@@ -0,0 +1,8 @@
+import z from "zod";
+
+const profileNameTypes = z.object({
+    firstName: z.string().min(2).regex(/^[a-zA-Z]+$/),
+    lastName:z.string().min(2).regex(/^[a-zA-Z]+$/),
+});
+
+export default profileNameTypes;
