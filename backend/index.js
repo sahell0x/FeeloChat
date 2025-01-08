5521c06c0128b192658e4fe0c72f18fc7e3da3c4
sahell0x <sahil.sk.khan6872@gmail.com>
2025-01-08 22:19:46 +0530

    needfull json size

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(cors({
 
 // app.use(cors({ origin: '*' }));
 
-app.use(express.json({ limit: '10mb' })); 
+app.use(express.json({ limit: '5mb' })); 
 
 app.use(cookieParser());
 app.use(express.json());
