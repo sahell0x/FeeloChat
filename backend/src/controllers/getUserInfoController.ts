c659d05ae88eeff53c912e4180e14e27187c8988
sahell0x <sahil.sk.khan6872@gmail.com>
2025-01-13 15:34:51 +0530

    updated tsconfig

--- backend/src/controllers/getUserInfoController.ts
+++ backend/src/controllers/getUserInfoController.ts
@@ -5,7 +5,7 @@ import { Request,Response } from "express";
 
 const getUserInfoController = async (req :Request,res :Response)=>{
     try{
-        const userId = req.userId;
+        const userId :string = req.userId;
 
         const userInfo = await User.findOne({_id:userId});
         if(!userInfo){
