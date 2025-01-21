1c103eb4ff6e3567fa6d6ea27b68b1530c40788a
sahell0x <sahil.sk.khan6872@gmail.com>
2025-01-21 17:47:50 +0530

    updated chat header

--- frontend/src/pages/chat/components/ui/ChatHeader.jsx
+++ frontend/src/pages/chat/components/ui/ChatHeader.jsx
@@ -1,17 +1,18 @@
-import {RiCloseFill} from "react-icons/ri";
+import { RiCloseFill } from "react-icons/ri";
+
 function ChatHeader() {
   return (
-    <div className='h-[10vh] border-b-2 border-gray-200 flex items-center justify-between px-20 '>
-        <div className="flex gap-5 items-center">
-            <div className="flex gap-3 items-center justify-center"></div>
-             <div className="flex items-center justify-center gap-5">
-                <button className="text-neutral-500 focus:border-none focus:text-black duration-300 transition-all">
-                    <RiCloseFill className="text-3xl"/>
-                </button>
-             </div>
+    <div className="h-[10vh] border-b-2 border-[#3a3b45] flex items-center justify-between px-20 bg-[#1b1c24]">
+      <div className="flex gap-5 items-center">
+        <div className="flex gap-3 items-center justify-center"></div>
+        <div className="flex items-center justify-center gap-5">
+          <button className="text-gray-400 hover:text-white transition-all duration-300">
+            <RiCloseFill className="text-3xl" />
+          </button>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default ChatHeader
+export default ChatHeader;
