5eab59aa24440157bf57e52bae7296fe1a043ba8
sahell0x <sahil.sk.khan6872@gmail.com>
2025-01-21 17:48:29 +0530

    updated message container

--- frontend/src/pages/chat/components/ui/MessageContainer.jsx
+++ frontend/src/pages/chat/components/ui/MessageContainer.jsx
@@ -1,17 +1,30 @@
-import Loader from '@/components/ui/Loader'
-import React from 'react'
+import Loader from "@/components/ui/Loader";
+import React from "react";
 
 function MessageContainer() {
   return (
-    <div id='me' className='flex-1 overflow-y-auto scrollbar-thin w-[100vw] h-[70vh] pb-11 p-4 px-8 md:w-[65vw] lg:w-[70vw] xl:w-[80vw]'>
-          Lorem ipsum dolnecessitatibus! Recusandae nam iste rem?
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam ab quaerat, repellat asperiores necessitatibus quis blanditiis vel libero voluptatem sed id architecto aperiam consectetur, quas nulla facilis, amet dignissimos vero!
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. At, doloribus optio voluptas iste rem voluptate laborum expedita nisi velit quibusdam suscipit praesentium animi ut ipsa excepturi doloremque
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae quae ad repellat magnam delectus alias commodi quibusdam possimus ex dignissimos? Inventore vero impedit hic eius suscipit autem tempora atque beatae
-          Lorem ipsr sit amet consectetur adipisicing elit. Aliquam ab quaerat, repellat asperiores necessitatibus quis blanditiis vel libero voluptatem sed id architecto aperiam consectetur, quas nulla facilis, amet dignissimos vero!
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. At, doloribus optio voluptas iste rem voluptate laborum expedita nisi velit quibusdam suscipit praesentium animi ut ipsa excepturi doloremque
+    <div
+      id="me"
+      className="flex-1 overflow-y-auto scrollbar-thin w-[100vw] h-[70vh] pb-11 p-4 px-8 md:w-[65vw] lg:w-[70vw] xl:w-[80vw] bg-[#1b1c24] text-white"
+    >
+      Lorem ipsum dolnecessitatibus! Recusandae nam iste rem? Lorem ipsum, dolor
+      sit amet consectetur adipisicing elit. Aliquam ab quaerat, repellat
+      asperiores necessitatibus quis blanditiis vel libero voluptatem sed id
+      architecto aperiam consectetur, quas nulla facilis, amet dignissimos vero!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. At, doloribus
+      optio voluptas iste rem voluptate laborum expedita nisi velit quibusdam
+      suscipit praesentium animi ut ipsa excepturi doloremque Lorem ipsum dolor
+      sit amet consectetur, adipisicing elit. Beatae quae ad repellat magnam
+      delectus alias commodi quibusdam possimus ex dignissimos? Inventore vero
+      impedit hic eius suscipit autem tempora atque beatae Lorem ipsr sit amet
+      consectetur adipisicing elit. Aliquam ab quaerat, repellat asperiores
+      necessitatibus quis blanditiis vel libero voluptatem sed id architecto
+      aperiam consectetur, quas nulla facilis, amet dignissimos vero! Lorem
+      ipsum dolor sit amet consectetur adipisicing elit. At, doloribus optio
+      voluptas iste rem voluptate laborum expedita nisi velit quibusdam suscipit
+      praesentium animi ut ipsa excepturi doloremque
     </div>
-  )
+  );
 }
 
-export default MessageContainer
+export default MessageContainer;
