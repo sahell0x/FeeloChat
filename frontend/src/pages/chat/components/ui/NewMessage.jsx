bcc037750747140403e56f59e667911a5ae73eaa
sahell0x <sahil.sk.khan6872@gmail.com>
2025-01-22 21:36:20 +0530

    added state for searched contacts

--- frontend/src/pages/chat/components/ui/NewMessage.jsx
+++ frontend/src/pages/chat/components/ui/NewMessage.jsx
@@ -14,6 +14,7 @@ import { Debounce } from "@/util/debounce";
 
 function NewMessage() {
     const [isContactDialogOpende, setIsContactDialogOpende] = useState(false);
+    const [searchedContacts,setSearchedContacts] = useState([]);
     
     const handleContactSearch = (query)=>{
       console.log(query);
