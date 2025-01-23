b04ec14437c8bf8488e8b605aab479370f5cbc8c
sahell0x <sahil.sk.khan6872@gmail.com>
2025-01-23 19:18:34 +0530

    updated wrapper with props

--- frontend/src/pages/chat/components/ui/ProfileWrapper.jsx
+++ frontend/src/pages/chat/components/ui/ProfileWrapper.jsx
@@ -3,24 +3,22 @@ import userInfoAtom from "@/stores/userInfoAtom";
 import { useRecoilState, useRecoilValue } from "recoil";
 import getFirstLetter from "@/util/getFirstLetter";
 
-function ProfileWrapper() {
-  const [userInfo , setUserInfo] = useRecoilState(userInfoAtom);
-
+function ProfileWrapper({firstName , lastName, img , email}) {
   return (
     <div className="flex items-center gap-4">
     <Avatar className="size-14 border-2 border-[#3a3b45]">
-      <AvatarImage src={userInfo.img} />
+      <AvatarImage src={img} />
       <AvatarFallback className="text-2xl bg-[#3a3b45] text-white">
-        {userInfo.firstName
-          ? getFirstLetter(userInfo.firstName)
-          : getFirstLetter(userInfo.email)}
+        {firstName
+          ? getFirstLetter(firstName)
+          : getFirstLetter(email)}
       </AvatarFallback>
     </Avatar>
     <div className="flex flex-col">
       <h3 className="text-lg font-semibold text-white">
-        {userInfo.firstName ? userInfo.firstName +" "+userInfo.lastName : "User"}
+        {firstName ? firstName +" "+lastName : "User"}
       </h3>
-      <p className="text-sm text-gray-400">{userInfo.email}</p>
+      <p className="text-sm text-gray-400">{email}</p>
     </div>
   </div>
   )
