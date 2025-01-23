8993f2df66edbcbeaf371346096c0044420d608f
sahell0x <sahil.sk.khan6872@gmail.com>
2025-01-23 22:34:17 +0530

    added chat close functionality

--- frontend/src/pages/chat/components/ui/ChatHeader.jsx
+++ frontend/src/pages/chat/components/ui/ChatHeader.jsx
@@ -1,12 +1,19 @@
+import chatAtom from "@/stores/chatAtom";
 import { RiCloseFill } from "react-icons/ri";
+import { useResetRecoilState } from "recoil";
 
 function ChatHeader() {
+
+  const chatStateReset = useResetRecoilState(chatAtom);
+
   return (
     <div className="h-[10vh] border-b-2 border-[#3a3b45] flex items-center justify-between px-20 bg-[#1b1c24]">
       <div className="flex gap-5 items-center">
         <div className="flex gap-3 items-center justify-center"></div>
         <div className="flex items-center justify-center gap-5">
-          <button className="text-gray-400 hover:text-white transition-all duration-300">
+          <button
+          onClick={()=>chatStateReset()}
+           className="text-gray-400 hover:text-white transition-all duration-300">
             <RiCloseFill className="text-3xl" />
           </button>
         </div>
