ea22633ec6321cfa05bcb9df1d93f0a9a4df87f5
sahell0x <sahil.sk.khan6872@gmail.com>
2025-01-23 20:31:52 +0530

    added loader in new message

--- frontend/src/pages/chat/components/ui/NewMessage.jsx
+++ frontend/src/pages/chat/components/ui/NewMessage.jsx
@@ -1,6 +1,8 @@
 import { Button } from "@/components/ui/button";
 import { Plus } from "lucide-react";
-import React from 'react';
+import { ClipLoader } from "react-spinners";
+
+import React from "react";
 import {
   Dialog,
   DialogContent,
@@ -20,84 +22,103 @@ import { ScrollArea } from "@/components/ui/scroll-area";
 import ProfileWrapper from "./ProfileWrapper";
 
 function NewMessage() {
-    const [isContactDialogOpende, setIsContactDialogOpende] = useState(false);
-    const [searchedContacts,setSearchedContacts] = useState([]);
-    
-    const handleContactSearch = async (query)=>{
-      try{
+  const [isContactDialogOpende, setIsContactDialogOpende] = useState(false);
+  const [searchedContacts, setSearchedContacts] = useState([]);
+  const [loading,isLoading] = useState(false);
 
-        if(query.length != 0){
-          const queryResponse = await apiClient.get(`${SEARCH_ROUTE}?query=${query}`,{withCredentials:true});
+  const handleContactSearch = async (query) => {
+    try {
+      if (query.length != 0) {
+        const queryResponse = await apiClient.get(
+          `${SEARCH_ROUTE}?query=${query}`,
+          { withCredentials: true }
+        );
 
-          const contacts = queryResponse.data.contacts;
-          if(queryResponse.status===200 && contacts.length){
-              setSearchedContacts([...contacts]);
-              console.log(contacts);
-          }else{
-            setSearchedContacts([]);
-          }
+        const contacts = queryResponse.data.contacts;
+        if (queryResponse.status === 200 && contacts.length) {
+          setSearchedContacts([...contacts]);
+          console.log(contacts);
+        } else {
+          setSearchedContacts([]);
         }
-
-      }catch{
-        setSearchedContacts([]);
       }
-      
-
+    } catch {
+      setSearchedContacts([]);
     }
-    const debouncer = new Debounce;
-    const debouncedHandleContactSearch =  debouncer.debounce(handleContactSearch,500);
+  };
+  const debouncer = new Debounce();
+  const debouncedHandleContactSearch = debouncer.debounce(
+    handleContactSearch,
+    500
+  );
 
-
-    const selectNewContact = (contact)=>{
-      console.log(contact);
-    }
+  const selectNewContact = (contact) => {
+    console.log(contact);
+  };
 
   return (
     <>
       <Button
         className="w-full bg-[#2c2e3b] text-white/90 hover:bg-[#3a3b45]  border-[#3a3b45] hover:text-white"
         variant="outline"
-        onClick={()=>setIsContactDialogOpende(true)}
+        onClick={() => setIsContactDialogOpende(true)}
       >
         <Plus className="h-4 w-4 mr-2" />
         New Message
       </Button>
 
-      <Dialog open={isContactDialogOpende} onOpenChange={setIsContactDialogOpende}>
+      <Dialog
+        open={isContactDialogOpende}
+        onOpenChange={setIsContactDialogOpende}
+      >
         <DialogContent className="bg-[#181920] border-none text-white w-[400px] h-[400px] flex flex-col">
           <DialogHeader>
-            <DialogTitle className="flex items-center justify-center">Select a contact</DialogTitle>
-            <DialogDescription>
-              
-            </DialogDescription>
+            <DialogTitle className="flex items-center justify-center">
+              Select a contact
+            </DialogTitle>
+            <DialogDescription></DialogDescription>
           </DialogHeader>
           <div>
-            <Input 
-            placeholder="Search Contacts"
-            className="rounded-lg p-6 bg-[#2c2e3b] text-white/90"
-            onChange={(e)=>debouncedHandleContactSearch(e.target.value.trim())}
-
-             />
-          </div>
-          <ScrollArea className="h-[250px]">
-            <div className="flex flex-col gap-5">
-              {searchedContacts.map((contact)=>{
-
-                return  <div className="cursor-pointer" onClick={()=>selectNewContact(contact)} key={contact._id}>
-                  <ProfileWrapper 
-                 firstName={contact.firstName} lastName={contact.lastName} email={contact.email} img={contact.img}  />
-                </div>
-
-              })
-               
+            <Input
+              placeholder="Search Contacts"
+              className="rounded-lg p-6 bg-[#2c2e3b] text-white/90"
+              onChange={(e) =>
+                debouncedHandleContactSearch(e.target.value.trim())
               }
-            </div>
-          </ScrollArea>
-          {searchedContacts.length ==0 && (
-             <div className="h-[100%] w-[100%] flex items-center justify-center flex-col">
-              <h1 className="text-xl md:text-2xl text-white/80">Please <span className="font-bold"><GradientWrapper>Search</GradientWrapper></span> for a  new contact</h1>
-             </div>
+            />
+          </div>
 
+          {searchedContacts.length == 0 ? (
+            <div className="h-[100%] w-[100%] flex items-center justify-center flex-col">
+              {loading ? <ClipLoader color="#9333ea" />  : (<h1 className="text-xl md:text-2xl text-white/80">
+                Please{" "}
+                <span className="font-bold">
+                  <GradientWrapper>Search</GradientWrapper>
+                </span>{" "}
+                for a new contact
+              </h1>)}
+            </div>
+          ) : (
+            <ScrollArea className="h-[250px]">
+              <div className="flex flex-col gap-5">
+                {searchedContacts.map((contact) => {
+                  return (
+                    <div
+                      className="cursor-pointer"
+                      onClick={() => selectNewContact(contact)}
+                      key={contact._id}
+                    >
+                      <ProfileWrapper
+                        firstName={contact.firstName}
+                        lastName={contact.lastName}
+                        email={contact.email}
+                        img={contact.img}
+                      />
+                    </div>
+                  );
+                })}
+              </div>
+            </ScrollArea>
           )}
         </DialogContent>
       </Dialog>
