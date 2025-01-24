720b724b709c39158facad1f7a78a1b3ee5150c2
sahell0x <sahil.sk.khan6872@gmail.com>
2025-01-24 17:59:36 +0530

    added message schema

---
+++ backend/src/models/messageModel.ts
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema(
+    {
+        sender: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: true,
+        },
+
+        receiver: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User', 
+            required: true,
+        },
+
+        content: {
+            type: String,
+            required: true,
+            trim: true, 
+        },
+
+        timestamp: {
+            type: Date,
+            default: Date.now, 
+        },
+
+        // isRead: {
+        //     type: Boolean,
+        //     default: false,
+        // },
+
+        // isDelivered: {
+        //     type: Boolean,
+        //     default: false,
+        // },
+    }
+);
+
+const Message = mongoose.model('Message', messageSchema);
+
+export default Message;
