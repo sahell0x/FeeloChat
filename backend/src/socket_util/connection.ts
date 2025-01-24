b859b48af768e3dbf722847beac70e9d2f56f6ae
sahell0x <sahil.sk.khan6872@gmail.com>
2025-01-24 16:33:49 +0530

    added connection handler

--- backend/src/socket_util/connection.ts
+++ backend/src/socket_util/connection.ts
@@ -1,1 +1,12 @@
-const 
+import { Socket } from "socket.io";
+
+const connection = (socket:Socket, userMap:Map<string,string>):void=>{
+   const userId = socket.handshake.query.userId as string;
+
+   if(userId){
+      userMap.set(userId,socket.id);
+      console.log(`user with id ${userId} is connected with socket id ${socket.id}`)
+   }else{
+        console.log("user id is not provided during connection");
+   }
+} 
