d789677517d1cb3638afe90d9458a4124f218a88
sahell0x <sahil.sk.khan6872@gmail.com>
2025-01-24 16:25:13 +0530

    disconnection types

--- backend/src/socket_util/disconnection.ts
+++ backend/src/socket_util/disconnection.ts
@@ -1,6 +1,6 @@
 import { Socket } from "socket.io";
 
-const disconnection = (socket:Socket , userMap: Map<string,string>)=>{
+const disconnection = (socket:Socket , userMap: Map<string,string>):void=> {
      console.log(`client disconnected ${socket.id}`);
 
      for(const [userId,socketId] of userMap.entries()){
