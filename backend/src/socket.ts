116ffc1294a681fca83d62b8bea45d15589d1fbb
sahell0x <sahil.sk.khan6872@gmail.com>
2025-01-24 12:48:09 +0530

    io server setup

--- backend/src/socket.ts
+++ backend/src/socket.ts
@@ -1,5 +1,14 @@
-const socketSetup = (server)=>{
-  
+import { Server } from "http";
+import { Server as SocketServer} from "socket.io";
+
+const socketSetup = (server:Server)=>{
+   const io = new SocketServer(server, {
+        cors:{
+            origin: process.env.ORIGIN,
+            methods:["GET","POST"],
+            credentials:true,
+        }
+   });
 }
 
 export default socketSetup;
