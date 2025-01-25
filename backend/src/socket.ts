f8a052daf83f7b22c9fa1b02391b35331a3a61d9
sahell0x <sahil.sk.khan6872@gmail.com>
2025-01-25 18:23:58 +0530

    added handle message

--- backend/src/socket.ts
+++ backend/src/socket.ts
@@ -2,6 +2,7 @@ import { Server } from "http";
 import { Server as SocketServer} from "socket.io";
 import disconnection from "./socket_util/disconnection";
 import connection from "./socket_util/connection";
+import Message from "./models/messageModel";
 
 const socketSetup = (server:Server)=>{
    const io:SocketServer = new SocketServer(server, {
@@ -13,6 +14,26 @@ const socketSetup = (server:Server)=>{
    });
 
    const userSocketMap = new Map();
+
+   const handleMessage = async ( message):Promise<void> => {
+    
+    const senderSocketId = userSocketMap.get(message.sender);
+
+    const recieverSocketId = userSocketMap.get(message.receiver);
+
+    const createdMessage = await Message.create(message);
+
+    const messageData = await Message.findById(createdMessage._id).populate("sender","id email firstName lastName img").populate("reciever","id email firstName lastName img");
+
+    if(recieverSocketId){
+        io.to(recieverSocketId).emit("recieveMessage",messageData);
+    }
+
+    if(senderSocketId){
+        io.to(senderSocketId).emit("recieveMessage",messageData);
+    }
+  
+}
    
    io.on("connection",(socket)=>{
       connection(socket,userSocketMap);
