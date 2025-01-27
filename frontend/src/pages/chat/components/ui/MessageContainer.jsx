4f6b2d55ce47e5f6ed5c29670395c28ff07025dc
sahell0x <sahil.sk.khan6872@gmail.com>
2025-01-27 14:59:10 +0530

    render message

--- frontend/src/pages/chat/components/ui/MessageContainer.jsx
+++ frontend/src/pages/chat/components/ui/MessageContainer.jsx
@@ -1,156 +1,50 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "../../../../App.css";
+import { useRecoilValue } from "recoil";
+import { selectedChatDataAtom, selectedChatMessagesAtom, selectedChatTypeAtom } from "@/stores/chatAtom";
+import moment from "moment";
 
 function MessageContainer() {
+  const selectedChatMessages = useRecoilValue(selectedChatMessagesAtom);
+  const selectedChatData = useRecoilValue(selectedChatDataAtom);
+  const selectedChatType = useRecoilValue(selectedChatTypeAtom);
+
+  const ScrollRef = useRef();
+
+ const renderMessages = ()=>{
+    let lastData = null;
+
+    return selectedChatMessages.map((message)=>{
+        const messageDate = moment(message.timestamp).format("YYYY-MM-DD");
+        const showData = messageDate !== lastData;
+
+        lastData = messageDate;
+
+        return (
+          <div key={message._id}>
+              {
+                 showData && (
+                   <div className="text-center text-gray-500 my-2">
+                    {moment(message.timestamp).format("LL")}
+                   </div>
+                 )
+              }
+          </div>
+        )
+    });
+ }
+
+  useEffect(()=>{
+   if(ScrollRef.current){
+      ScrollRef.current.scrollIntoView({behavior:"smooth"});
+   }
+  },[selectedChatMessages]);
   return (
     <div
-      id="me"
-      className="flex-1 overflow-y-auto scrollbar-thin scrollbar-thumb-[#3a3b45] scrollbar-track-transparent hover:scrollbar-thumb-[#4c4d5c] w-[100vw] h-[70vh] pb-11 p-4 px-8 md:w-[65vw] lg:w-[70vw] xl:w-[80vw] bg-[#1b1c24] text-white"
+      className="flex-1 overflow-y-auto h-[70vh] bg-[#1b1c24] text-white scrollbar-thin scrollbar-thumb-[#3a3b45] scrollbar-track-transparent hover:scrollbar-thumb-[#4c4d5c] w-[100vw]  pb-11 p-4 px-8 md:w-[65vw] lg:w-[70vw] xl:w-[80vw]"
     >
-      Lorem ipsum dolnecessitatibus! Recusandae nam iste rem? Lorem ipsum, dolor
-      sit amet consectetur adipisicing elit. Aliquam ab quaerat, repellat
-      asperiores necessitatibus quis blanditiis vel libero voluptatem sed id
-      architecto aperiam consectetur, quas nulla facilis, amet dignissimos vero!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. At, doloribus
-      optio voluptas iste rem voluptate laborum expedita nisi velit quibusdam
-      suscipit praesentium animi ut ipsa excepturi doloremque Lorem ipsum dolor
-      sit amet consectetur, adipisicing elit. Beatae quae ad repellat magnam
-      delectus alias commodi quibusdam possimus ex dignissimos? Inventore vero
-      impedit hic eius suscipit autem tempora atque beatae Lorem ipsr sit amet
-      consectetur adipisicing elit. Aliquam ab quaerat, repellat asperiores
-      necessitatibus quis blanditiis vel libero voluptatem sed id architecto
-      aperiam consectetur, quas nulla facilis, amet dignissimos vero! Lorem
-      ipsum dolor sit amet consectetur adipisicing elit. At, doloribus optio
-      voluptas iste rem voluptate laborum expedita nisi velit quibusdam suscipit
-      praesentium animi ut ipsa excepturi doloremque
-      Lorem ipsum dolnecessitatibus! Recusandae nam iste rem? Lorem ipsum, dolor
-      sit amet consectetur adipisicing elit. Aliquam ab quaerat, repellat
-      asperiores necessitatibus quis blanditiis vel libero voluptatem sed id
-      architecto aperiam consectetur, quas nulla facilis, amet dignissimos vero!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. At, doloribus
-      optio voluptas iste rem voluptate laborum expedita nisi velit quibusdam
-      suscipit praesentium animi ut ipsa excepturi doloremque Lorem ipsum dolor
-      sit amet consectetur, adipisicing elit. Beatae quae ad repellat magnam
-      delectus alias commodi quibusdam possimus ex dignissimos? Inventore vero
-      impedit hic eius suscipit autem tempora atque beatae Lorem ipsr sit amet
-      consectetur adipisicing elit. Aliquam ab quaerat, repellat asperiores
-      necessitatibus quis blanditiis vel libero voluptatem sed id architecto
-      aperiam consectetur, quas nulla facilis, amet dignissimos vero! Lorem
-      ipsum dolor sit amet consectetur adipisicing elit. At, doloribus optio
-      voluptas iste rem voluptate laborum expedita nisi velit quibusdam suscipit
-      praesentium animi ut ipsa 
-      Lorem ipsum dolnecessitatibus! Recusandae nam iste rem? Lorem ipsum, dolor
-      sit amet consectetur adipisicing elit. Aliquam ab quaerat, repellat
-      asperiores necessitatibus quis blanditiis vel libero voluptatem sed id
-      architecto aperiam consectetur, quas nulla facilis, amet dignissimos vero!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. At, doloribus
-      optio voluptas iste rem voluptate laborum expedita nisi velit quibusdam
-      suscipit praesentium animi ut ipsa excepturi doloremque Lorem ipsum dolor
-      sit amet consectetur, adipisicing elit. Beatae quae ad repellat magnam
-      delectus alias commodi quibusdam possimus ex dignissimos? Inventore vero
-      impedit hic eius suscipit autem tempora atque beatae Lorem ipsr sit amet
-      consectetur adipisicing elit. Aliquam ab quaerat, repellat asperiores
-      necessitatibus quis blanditiis vel libero voluptatem sed id architecto
-      aperiam consectetur, quas nulla facilis, amet dignissimos vero! Lorem
-      ipsum dolor sit amet consectetur adipisicing elit. At, doloribus optio
-      voluptas iste rem voluptate laborum expedita nisi velit quibusdam suscipit
-      praesentium animi ut ipsa 
-      Lorem ipsum dolnecessitatibus! Recusandae nam iste rem? Lorem ipsum, dolor
-      sit amet consectetur adipisicing elit. Aliquam ab quaerat, repellat
-      asperiores necessitatibus quis blanditiis vel libero voluptatem sed id
-      architecto aperiam consectetur, quas nulla facilis, amet dignissimos vero!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. At, doloribus
-      optio voluptas iste rem voluptate laborum expedita nisi velit quibusdam
-      suscipit praesentium animi ut ipsa excepturi doloremque Lorem ipsum dolor
-      sit amet consectetur, adipisicing elit. Beatae quae ad repellat magnam
-      delectus alias commodi quibusdam possimus ex dignissimos? Inventore vero
-      impedit hic eius suscipit autem tempora atque beatae Lorem ipsr sit amet
-      consectetur adipisicing elit. Aliquam ab quaerat, repellat asperiores
-      necessitatibus quis blanditiis vel libero voluptatem sed id architecto
-      aperiam consectetur, quas nulla facilis, amet dignissimos vero! Lorem
-      ipsum dolor sit amet consectetur adipisicing elit. At, doloribus optio
-      voluptas iste rem voluptate laborum expedita nisi velit quibusdam suscipit
-      praesentium animi ut ipsa 
-      Lorem ipsum dolnecessitatibus! Recusandae nam iste rem? Lorem ipsum, dolor
-      sit amet consectetur adipisicing elit. Aliquam ab quaerat, repellat
-      asperiores necessitatibus quis blanditiis vel libero voluptatem sed id
-      architecto aperiam consectetur, quas nulla facilis, amet dignissimos vero!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. At, doloribus
-      optio voluptas iste rem voluptate laborum expedita nisi velit quibusdam
-      suscipit praesentium animi ut ipsa excepturi doloremque Lorem ipsum dolor
-      sit amet consectetur, adipisicing elit. Beatae quae ad repellat magnam
-      delectus alias commodi quibusdam possimus ex dignissimos? Inventore vero
-      impedit hic eius suscipit autem tempora atque beatae Lorem ipsr sit amet
-      consectetur adipisicing elit. Aliquam ab quaerat, repellat asperiores
-      necessitatibus quis blanditiis vel libero voluptatem sed id architecto
-      aperiam consectetur, quas nulla facilis, amet dignissimos vero! Lorem
-      ipsum dolor sit amet consectetur adipisicing elit. At, doloribus optio
-      voluptas iste rem voluptate laborum expedita nisi velit quibusdam suscipit
-      praesentium animi ut ipsa 
-      Lorem ipsum dolnecessitatibus! Recusandae nam iste rem? Lorem ipsum, dolor
-      sit amet consectetur adipisicing elit. Aliquam ab quaerat, repellat
-      asperiores necessitatibus quis blanditiis vel libero voluptatem sed id
-      architecto aperiam consectetur, quas nulla facilis, amet dignissimos vero!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. At, doloribus
-      optio voluptas iste rem voluptate laborum expedita nisi velit quibusdam
-      suscipit praesentium animi ut ipsa excepturi doloremque Lorem ipsum dolor
-      sit amet consectetur, adipisicing elit. Beatae quae ad repellat magnam
-      delectus alias commodi quibusdam possimus ex dignissimos? Inventore vero
-      impedit hic eius suscipit autem tempora atque beatae Lorem ipsr sit amet
-      consectetur adipisicing elit. Aliquam ab quaerat, repellat asperiores
-      necessitatibus quis blanditiis vel libero voluptatem sed id architecto
-      aperiam consectetur, quas nulla facilis, amet dignissimos vero! Lorem
-      ipsum dolor sit amet consectetur adipisicing elit. At, doloribus optio
-      voluptas iste rem voluptate laborum expedita nisi velit quibusdam suscipit
-      praesentium animi ut ipsa 
-      Lorem ipsum dolnecessitatibus! Recusandae nam iste rem? Lorem ipsum, dolor
-      sit amet consectetur adipisicing elit. Aliquam ab quaerat, repellat
-      asperiores necessitatibus quis blanditiis vel libero voluptatem sed id
-      architecto aperiam consectetur, quas nulla facilis, amet dignissimos vero!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. At, doloribus
-      optio voluptas iste rem voluptate laborum expedita nisi velit quibusdam
-      suscipit praesentium animi ut ipsa excepturi doloremque Lorem ipsum dolor
-      sit amet consectetur, adipisicing elit. Beatae quae ad repellat magnam
-      delectus alias commodi quibusdam possimus ex dignissimos? Inventore vero
-      impedit hic eius suscipit autem tempora atque beatae Lorem ipsr sit amet
-      consectetur adipisicing elit. Aliquam ab quaerat, repellat asperiores
-      necessitatibus quis blanditiis vel libero voluptatem sed id architecto
-      aperiam consectetur, quas nulla facilis, amet dignissimos vero! Lorem
-      ipsum dolor sit amet consectetur adipisicing elit. At, doloribus optio
-      voluptas iste rem voluptate laborum expedita nisi velit quibusdam suscipit
-      praesentium animi ut ipsa Lorem ipsum dolnecessitatibus! Recusandae nam iste rem? Lorem ipsum, dolor
-      sit amet consectetur adipisicing elit. Aliquam ab quaerat, repellat
-      asperiores necessitatibus quis blanditiis vel libero voluptatem sed id
-      architecto aperiam consectetur, quas nulla facilis, amet dignissimos vero!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. At, doloribus
-      optio voluptas iste rem voluptate laborum expedita nisi velit quibusdam
-      suscipit praesentium animi ut ipsa excepturi doloremque Lorem ipsum dolor
-      sit amet consectetur, adipisicing elit. Beatae quae ad repellat magnam
-      delectus alias commodi quibusdam possimus ex dignissimos? Inventore vero
-      impedit hic eius suscipit autem tempora atque beatae Lorem ipsr sit amet
-      consectetur adipisicing elit. Aliquam ab quaerat, repellat asperiores
-      necessitatibus quis blanditiis vel libero voluptatem sed id architecto
-      aperiam consectetur, quas nulla facilis, amet dignissimos vero! Lorem
-      ipsum dolor sit amet consectetur adipisicing elit. At, doloribus optio
-      voluptas iste rem voluptate laborum expedita nisi velit quibusdam suscipit
-      praesentium animi ut ipsa 
-      Lorem ipsum dolnecessitatibus! Recusandae nam iste rem? Lorem ipsum, dolor
-      sit amet consectetur adipisicing elit. Aliquam ab quaerat, repellat
-      asperiores necessitatibus quis blanditiis vel libero voluptatem sed id
-      architecto aperiam consectetur, quas nulla facilis, amet dignissimos vero!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. At, doloribus
-      optio voluptas iste rem voluptate laborum expedita nisi velit quibusdam
-      suscipit praesentium animi ut ipsa excepturi doloremque Lorem ipsum dolor
-      sit amet consectetur, adipisicing elit. Beatae quae ad repellat magnam
-      delectus alias commodi quibusdam possimus ex dignissimos? Inventore vero
-      impedit hic eius suscipit autem tempora atque beatae Lorem ipsr sit amet
-      consectetur adipisicing elit. Aliquam ab quaerat, repellat asperiores
-      necessitatibus quis blanditiis vel libero voluptatem sed id architecto
-      aperiam consectetur, quas nulla facilis, amet dignissimos vero! Lorem
-      ipsum dolor sit amet consectetur adipisicing elit. At, doloribus optio
-      voluptas iste rem voluptate laborum expedita nisi velit quibusdam suscipit
-      praesentium animi ut ipsa 
-
+        {renderMessages()}
+      <div ref={ScrollRef} />
     </div>
   );
 }
