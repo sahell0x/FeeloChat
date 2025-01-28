86022e6f423ae06cafcd8975db811a650ee5b45b
sahell0x <sahil.sk.khan6872@gmail.com>
2025-01-28 12:15:18 +0530

    updated message container ui

--- frontend/src/pages/chat/components/ui/MessageBar.jsx
+++ frontend/src/pages/chat/components/ui/MessageBar.jsx
@@ -6,6 +6,8 @@ import { useRecoilValue } from "recoil";
 import  { selectedChatDataAtom, selectedChatTypeAtom } from "@/stores/chatAtom";
 import { useSocket } from "@/context/SocketContext";
 import userInfoAtom from "@/stores/userInfoAtom";
+import { SiOclif } from "react-icons/si";
+import toast from "react-hot-toast";
 
 function MessageBar() {
   const [message, setMessage] = useState("");
@@ -33,14 +35,24 @@ function MessageBar() {
   };
 
   const handleSend = ()=>{
-    if(selectedChatType === "contact" && message.length){
-      socket.emit("sendMessage",{
-         sender: userInfo.id,
-         content: message,
-         receiver : selectedChatData._id,
 
-      });
-    }
+      if(selectedChatType === "contact" && message.length){
+        
+        if(socket.connected){
+          socket.emit("sendMessage",{
+            sender: userInfo.id,
+            content: message,
+            receiver : selectedChatData._id,
+   
+         });
+
+            setMessage("");
+            
+        }else{
+          toast.error("Please check you internet connection.");
+        }
+      }
+   
   }
 
   useEffect(() => {
@@ -56,7 +68,7 @@ function MessageBar() {
   }, [isEmojiPickerOpend]);
 
   return (
-    <div className="h-[30vh] flex justify-center items-center px-5 bg-[#1b1c24]">
+    <div className="h-[20vh] flex justify-center items-center px-5 bg-[#1b1c24]">
       <div className="flex-1 flex rounded-md items-center justify-center flex-row">
         <div className="flex items-center justify-center flex-row">
           <textarea
@@ -65,7 +77,9 @@ function MessageBar() {
             className="flex-1 resize-none scrollbar-none overflow-y-auto p-4 pr-12 rounded-md w-[70vw] md:w-[50vw] lg:w-[50vw] xl:w-[50vw] bg-[#2c2e3b] text-white placeholder:text-gray-400 border border-[#3a3b45]"
             placeholder="Type a Message..."
             value={message}
-            onChange={(e) => setMessage(e.target.value)}
+            onChange={(e) => {setMessage(e.target.value)
+              console.dir(e.target);
+            }}
           />
 
           <div className="relative right-10 flex items-center justify-center">
--- frontend/src/pages/chat/components/ui/MessageContainer.jsx
+++ frontend/src/pages/chat/components/ui/MessageContainer.jsx
@@ -1,19 +1,40 @@
 import React, { useEffect, useRef } from "react";
 import "../../../../App.css";
-import { useRecoilValue } from "recoil";
-import { selectedChatDataAtom, selectedChatMessagesAtom, selectedChatTypeAtom } from "@/stores/chatAtom";
+import { useRecoilState, useRecoilValue } from "recoil";
+import {
+  selectedChatDataAtom,
+  selectedChatMessagesAtom,
+  selectedChatTypeAtom,
+} from "@/stores/chatAtom";
 import moment from "moment";
+import apiClient from "@/lib/api-client";
+import { MESSAGE_ROUTE } from "@/util/constants";
 
 function MessageContainer() {
-  const selectedChatMessages = useRecoilValue(selectedChatMessagesAtom);
+
+
+  const [selectedChatMessages, setSelectedChatMessages] = useRecoilState(
+    selectedChatMessagesAtom
+  );
+
   const selectedChatData = useRecoilValue(selectedChatDataAtom);
   const selectedChatType = useRecoilValue(selectedChatTypeAtom);
 
-  const containerRef = useRef(null); 
+  const containerRef = useRef(null);
 
   const renderDm = (message) => (
-    <div className={`${message.sender === selectedChatData._id ? "text-left" : "text-right"}`}>
-      <div className={`${message.sender !== selectedChatData._id ? "bg-[#8417ff]/5 text-[#8417ff]/90 border-[#8417ff]/50" : "bg-[#2a2b33]/5 text-white/80  border-white/20"} border inline-block p-4 rounded my-1 max-w-[50%] break-words`}>
+    <div
+      className={`${
+        message.sender === selectedChatData._id ? "text-left" : "text-right"
+      }`}
+    >
+      <div
+        className={`${
+          message.sender !== selectedChatData._id
+            ? "bg-[#8417ff]/5 text-[#8417ff]/90 border-[#8417ff]/50"
+            : "bg-[#2a2b33]/5 text-white/80  border-white/20"
+        } border inline-block p-4 rounded my-1 max-w-[50%] break-words`}
+      >
         {message.content}
       </div>
       <div className="text-xs text-gray-600">
@@ -44,6 +65,28 @@ function MessageContainer() {
     });
   };
 
+//this effect  is for fetch messages when ever a chat is selected
+
+  useEffect(()=>{
+
+    const getMessages = async ()=>{
+          try{
+
+            const response = await apiClient.get(`${MESSAGE_ROUTE}?_id=${selectedChatData._id}`,{withCredentials:true});
+
+            setSelectedChatMessages(response.data.messages);
+
+          }catch(e){
+            console.log(e);
+          }
+    }
+     
+    if(selectedChatData._id && selectedChatType==="contact"){
+      getMessages();
+    }
+
+  },[selectedChatData,selectedChatType]);
+
   useEffect(() => {
     if (containerRef.current) {
       containerRef.current.scrollTop = containerRef.current.scrollHeight;
@@ -52,8 +95,8 @@ function MessageContainer() {
 
   return (
     <div
-      ref={containerRef} 
-      className="flex-1 overflow-y-auto scroll-smooth h-[70vh] bg-[#1b1c24] text-white scrollbar-none scrollbar-thumb-[#3a3b45] scrollbar-track-transparent hover:scrollbar-thumb-[#4c4d5c] w-[100vw] pb-11 p-4 px-8 md:w-[65vw] lg:w-[70vw] xl:w-[80vw]"
+      ref={containerRef}
+      className="flex-1 overflow-y-auto scroll-smooth h-[75vh] bg-[#1b1c24] text-white scrollbar-none scrollbar-thumb-[#3a3b45] scrollbar-track-transparent hover:scrollbar-thumb-[#4c4d5c] w-[100vw] pb-11 p-4 px-8 md:w-[65vw] lg:w-[70vw] xl:w-[80vw]"
     >
       {renderMessages()}
     </div>
