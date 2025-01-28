95c96afb2b150233d81fc165e0da69ea2324166f
sahell0x <sahil.sk.khan6872@gmail.com>
2025-01-28 13:17:48 +0530

    styling refactor

--- frontend/src/pages/chat/components/ui/MessageContainer.jsx
+++ frontend/src/pages/chat/components/ui/MessageContainer.jsx
@@ -24,23 +24,24 @@ function MessageContainer() {
 
   const renderDm = (message) => (
     <div
-      className={`${
-        message.sender === selectedChatData._id ? "text-left" : "text-right"
-      }`}
-    >
-      <div
-        className={`${
-          message.sender !== selectedChatData._id
-            ? "bg-[#8417ff]/5 text-[#8417ff]/90 border-[#8417ff]/50"
-            : "bg-[#2a2b33]/5 text-white/80  border-white/20"
-        } border inline-block p-4 rounded my-1 max-w-[50%] break-words`}
-      >
-        {message.content}
-      </div>
-      <div className="text-xs text-gray-600">
-        {moment(message.timestamp).format("LT")}
-      </div>
-    </div>
+  className={`${
+    message.sender === selectedChatData._id ? "text-left" : "text-right"
+  }`}
+>
+  <div
+    className={`${
+      message.sender !== selectedChatData._id
+        ? "bg-purple-700 text-white" 
+        : "bg-[#2c2e36] text-[#e5e5e5] "
+    }  inline-block p-4 rounded-lg my-1 max-w-[50%] break-words `}
+  >
+    {message.content}
+  </div>
+  <div className="text-xs text-gray-400">
+    {moment(message.timestamp).format("LT")}
+  </div>
+</div>
+
   );
 
   const renderMessages = () => {
