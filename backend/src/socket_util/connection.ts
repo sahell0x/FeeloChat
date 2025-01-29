a968e0f49171bfb8540202eb31f7549f3d15cd15
sahell0x <sahil.sk.khan6872@gmail.com>
2025-01-29 15:22:14 +0530

    added online status provider on connection and discconention

--- backend/src/socket_util/connection.ts
+++ backend/src/socket_util/connection.ts
@@ -4,9 +4,12 @@ import onlineStatusProvider from "./onlineStatusProvider";
 //this func handles user connection and menage map with user id and user socket id adds id into the map.
 
 const connection = (socket:Socket, userMap:Map<string,string>,io:any):void=>{
+   const IS_USER_ONLINE = true; //connection frame
+
    const userId = socket.handshake.query.userId as string;
 
-   onlineStatusProvider(userId,io,userMap,true);
+   onlineStatusProvider(userId,io,userMap,IS_USER_ONLINE);
+
    if(userId){
       userMap.set(userId,socket.id);
    }
--- backend/src/socket_util/disconnection.ts
+++ backend/src/socket_util/disconnection.ts
@@ -1,13 +1,16 @@
 import { Socket } from "socket.io";
+import onlineStatusProvider from "./onlineStatusProvider";
 
 //this func handles user disconnection and menage map with user id and user socket id removes user id from the map.
 
 
 const disconnection = (socket:Socket , userMap: Map<string,string>,io:any):void=> {
-     console.log(`client disconnected ${socket.id}`);
+
+     const IS_USER_ONLINE = false;  //disconnection frame 
 
      const userId = socket.handshake.query.userId as string;     
      if(userId){
+        onlineStatusProvider(userId,io,userMap,IS_USER_ONLINE);
         userMap.delete(userId);
         
      }       
