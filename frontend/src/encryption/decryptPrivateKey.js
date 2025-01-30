0581d563afb266dd2bcee15be0a43498bb6f7f85
sahell0x <sahil.sk.khan6872@gmail.com>
2025-01-30 21:48:18 +0530

    added decrypt private key function

---
+++ frontend/src/encryption/decryptPrivateKey.js
@@ -0,0 +1,37 @@
+ 
+ 
+ const decryptPrivateKey = async (userPassword, encryptedPrivateKey, salt, nonce) => {
+
+    // encrypted private key salt and nonce will fetched from db at the time of login
+
+    await sodium.ready;
+  
+    // Convert Base64 strings to Uint8Arrays
+
+    const encrypted = sodium.from_base64(encryptedPrivateKey);
+    salt = sodium.from_base64(salt);
+    nonce = sodium.from_base64(nonce);
+  
+    // Re derive the encryption key using the password and stored salt
+
+    const derivedKey = sodium.crypto_pwhash(
+      sodium.crypto_secretbox_KEYBYTES,
+      userPassword,
+      salt,
+      sodium.crypto_pwhash_OPSLIMIT_MODERATE,
+      sodium.crypto_pwhash_MEMLIMIT_MODERATE,
+      sodium.crypto_pwhash_ALG_ARGON2ID13
+    );
+  
+    // Decrypt the private key
+    const privateKey = sodium.crypto_secretbox_open_easy(
+      encrypted,
+      nonce,
+      derivedKey
+    );
+  
+    return privateKey; // Uint8Array memory
+  }
+
+  
+  export default decryptPrivateKey;
