d1521a1763ed35020c37fee4e554506a6f199000
sahell0x <sahil.sk.khan6872@gmail.com>
2025-01-31 17:18:39 +0530

    added base64 to uint8 converter

---
+++ frontend/src/encryption/base64ToUint8Converter.js
@@ -0,0 +1,15 @@
+import sodium from 'libsodium-wrappers';
+
+//this function converts base64 string into uint8 array.
+
+const base64ToUint8Converter = async(base64Input)=>{
+
+        await sodium.ready;
+
+        const unit8ConvertedOutput = sodium.to_base64(base64Input);
+
+        return unit8ConvertedOutput;  // Uint8 converted array
+    
+}
+
+export default base64ToUint8Converter;
