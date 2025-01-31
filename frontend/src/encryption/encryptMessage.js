a761c2163aaa119430071be26bd70d4d77289a5e
sahell0x <sahil.sk.khan6872@gmail.com>
2025-01-31 17:25:07 +0530

    error handling in encrypt message

--- frontend/src/encryption/encryptMessage.js
+++ frontend/src/encryption/encryptMessage.js
@@ -2,27 +2,33 @@ import sodium from 'libsodium-wrappers';
 
  
  const encryptMessage = async(message, receiverPublicKey, senderPrivateKey)=> {
-    await sodium.ready;
+    try{
+        await sodium.ready;
   
-    // Convert receiver public key to Uint8 from base64
-
-    const receiverPublicKeyUint8 = sodium.from_base64(receiverPublicKey);
-  
-    // Generate a random nonce
-    const nonce = sodium.randombytes_buf(sodium.crypto_box_NONCEBYTES);
-  
-    // Encrypt the message
-    const ciphertext = sodium.crypto_box_easy(
-      message, // plain text from input field
-      nonce,
-      receiverPublicKeyUint8,
-      senderPrivateKey // sender private key in uint 8 from from user browser
-    );
-  
-    return {
-      ciphertext: sodium.to_base64(ciphertext),  //converted into base64 to store and transfer
-      nonce: sodium.to_base64(nonce)  //nonce will be used at the time of decryption
-    };
+        // Convert receiver public key to Uint8 from base64
+    
+        const receiverPublicKeyUint8 = sodium.from_base64(receiverPublicKey);
+      
+        // Generate a random nonce
+        const nonce = sodium.randombytes_buf(sodium.crypto_box_NONCEBYTES);
+      
+        // Encrypt the message
+        const ciphertext = sodium.crypto_box_easy(
+          message, // plain text from input field
+          nonce,
+          receiverPublicKeyUint8,
+          senderPrivateKey // sender private key in uint 8 from from user browser
+        );
+      
+        return {
+          ciphertext: sodium.to_base64(ciphertext),  //converted into base64 to store and transfer
+          nonce: sodium.to_base64(nonce)  //nonce will be used at the time of decryption
+        };
+    }catch{
+        return {
+            message:"encryption faild",
+        };
+    }
   }
 
 
