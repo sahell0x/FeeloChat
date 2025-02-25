2dfc9c4ed0242ffea13a6b30558c42f2d4b5e700
sahell0x <sahil.sk.khan6872@gmail.com>
2025-02-25 23:21:40 +0530

    header ui

--- frontend/src/pages/chat/components/ui/ChatHeader.jsx
+++ frontend/src/pages/chat/components/ui/ChatHeader.jsx
@@ -59,7 +59,7 @@ function ChatHeader() {
           }`}
         />
       </div>
-      <div className="flex gap-3 items-center">
+      <div className="flex gap-2 items-center md:gap-3">
         <TooltipProvider>
           <Tooltip>
             <TooltipTrigger asChild>
