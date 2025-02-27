101151a9d0e9ced348b225f1d542cdb6f3e0dd19
sahell0x <sahil.sk.khan6872@gmail.com>
2025-02-27 17:10:10 +0530

    recaptcha 3

--- backend/src/controllers/guestAcountController.ts
+++ backend/src/controllers/guestAcountController.ts
@@ -1,4 +1,4 @@
-import { RecaptchaResponse } from './../types/captchaTypes';
+import { RecaptchaResponse } from "./../types/captchaTypes";
 import jwt from "jsonwebtoken";
 import { StatusCode } from "status-code-enum";
 import User from "../models/userModel.js";
@@ -14,21 +14,26 @@ const guestAcountController = async (
   res: Response
 ): Promise<any> => {
   try {
-    const {publicKey,deviceFingerPrint,captchaValue} = req.body;
+    const { publicKey, deviceFingerPrint, captchaValue } = req.body;
     const secret = process.env.SECRET as string;
     const RECAPTCHA_SITE_SECRET = process.env.RECAPTCHA_SITE_SECRET as string;
 
-    const reponseData :any = await axios.post(
-      `https://www.google.com/recaptcha/api/siteverify?secret=${RECAPTCHA_SITE_SECRET}&response=${captchaValue}`,
+    const reponseData: any = await axios.post(
+      `https://www.google.com/recaptcha/api/siteverify?secret=${RECAPTCHA_SITE_SECRET}&response=${captchaValue}`
     );
 
-    const data : RecaptchaResponse = reponseData.data
+    const data: RecaptchaResponse = reponseData.data;
 
-    if(!data.success){
-      throw new Error("Captcha validation error");
+
+    if (!data.success) {
+      res.status(StatusCode.ClientErrorForbidden).json({
+        message :" captcha varification failed",
+      })
     }
 
-    const isFingerPrintExist = await GuestDeviceFingerPrint.findOne({ userDeviceFingerPrint: deviceFingerPrint });
+    const isFingerPrintExist = await GuestDeviceFingerPrint.findOne({
+      userDeviceFingerPrint: deviceFingerPrint,
+    });
 
     if (isFingerPrintExist) {
       return res.status(StatusCode.ClientErrorConflict).json({
@@ -36,42 +41,46 @@ const guestAcountController = async (
       });
     }
 
-    const response = await GuestDeviceFingerPrint.create({ userDeviceFingerPrint: deviceFingerPrint });
+    const response = await GuestDeviceFingerPrint.create({
+      userDeviceFingerPrint: deviceFingerPrint,
+    });
 
     if (!response) {
       throw new Error("DB opration failed");
     }
 
     const GuestUserData = {
-        email : guestUUIDProvider(),
-        publicKey:publicKey,
-        isGuest : true,
-        profileSetup:true,
-    }
+      email: guestUUIDProvider(),
+      publicKey: publicKey,
+      isGuest: true,
+      profileSetup: true,
+    };
 
     const guestUser = await User.create(GuestUserData);
 
-    if(!guestUser){
-        throw new Error("Error while creating");
+    if (!guestUser) {
+      throw new Error("Error while creating");
     }
 
-   const token = jwt.sign({ email: guestUser.email, id: guestUser._id }, secret);
+    const token = jwt.sign(
+      { email: guestUser.email, id: guestUser._id },
+      secret
+    );
 
-    res.cookie("token", token, {     
-        httpOnly: true,
-        sameSite: "none",
-        secure: true,
-        maxAge: 24 * 60 * 60 * 1000,   
+    res.cookie("token", token, {
+      httpOnly: true,
+      sameSite: "none",
+      secure: true,
+      maxAge: 24 * 60 * 60 * 1000,
     });
 
     return res.status(StatusCode.SuccessCreated).json({
-        email: guestUser.email,
-        id: guestUser._id,
-        publicKey:guestUser.publicKey,
-        isGuest:guestUser.isGuest,
-        profileSetup:guestUser.profileSetup,
+      email: guestUser.email,
+      id: guestUser._id,
+      publicKey: guestUser.publicKey,
+      isGuest: guestUser.isGuest,
+      profileSetup: guestUser.profileSetup,
     });
-
   } catch {
     res.status(StatusCode.ClientErrorBadRequest).json({
       message: "Bad request.",
